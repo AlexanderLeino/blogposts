3e4a4288b7fabd7af6d378e7ddf1f9e995328d8e
Alexander Leino <Alexander.Leino@yahoo.com>
2021-10-27 18:04:53 -0400

    updated ui secondary header

--- controllers/index.js
+++ controllers/index.js
@@ -1,48 +1,53 @@
 const router = require('express').Router()
-const slugify = require('slugify')
+// const slugify = require('slugify')
 
 
 // html routes
-router.get('/', (req, res) => {
-  res.render('index', { 
-    name: 'Josh',
-    date: new Date()
-  })
-})
+router.get('/', (req, res) =>{
 
-router.get('/about', (req, res) => res.render('about'))
+ res.render('home', {
+     secondarytitle:"The Tech Blog"
 
-router.get('/contact', (req, res) => res.render('contact'))
+ })})
 
-router.get('/services', (req, res) => res.render('services'))
+router.get('/dashboard', (req, res) => res.render('dashboard', {
+  secondarytitle:'Your Dashboard'  
+}))
 
-router.get('/add-post', (req, res) => {
-  res.render('add-post')
-})
+router.get('/logout', (req, res) => res.render('logout'))
 
-router.get('/blog', (req, res) => {
-  res.render('blog', {
-    post: blogPosts.map(post => {
+
+router.get('/login', (req, res) => res.render('login', {
+    secondarytitle: 'The Tech Blog'
+}))
+
+// router.get('/add-post', (req, res) => {
+//   res.render('add-post')
+// })
+
+// router.get('/blog', (req, res) => {
+//   res.render('blog', {
+//     post: blogPosts.map(post => {
       
-      const slug = slugify(post.name, { lower: true })
-      post.slug = slug
-      return post
-    })
-  })
-})
+//       const slug = slugify(post.name, { lower: true })
+//       post.slug = slug
+//       return post
+//     })
+//   })
+// })
 
-router.get('/blog/:slug', (req, res) => {
-  const postObj = blogPosts
-    .find(post => req.params.slug === post.slug)
+// router.get('/blog/:slug', (req, res) => {
+//   const postObj = blogPosts
+//     .find(post => req.params.slug === post.slug)
 
-  res.render('post', postObj)
-})
+//   res.render('post', postObj)
+// })
 
-router.post('/api/blogpost', (req, res) => {
-  const body = req.body
-  body.date = new Date()
-  blogPosts.push(body)
-  res.status(200).send('Post added succesfully!')
-})
+// router.post('/api/blogpost', (req, res) => {
+//   const body = req.body
+//   body.date = new Date()
+//   blogPosts.push(body)
+//   res.status(200).send('Post added succesfully!')
+// })
 
 module.exports = router
